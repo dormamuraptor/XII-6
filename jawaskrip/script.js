8cda3d0e4c94bf88cd72392f71864d0a972449e8
dormamuraptor <nayfauzya@gmail.com>
2024-10-14 08:26:46 +0700

    komit pertama

---
+++ jawaskrip/script.js
@@ -0,0 +1,9 @@
+for (noRuang = 1; noRuang <= 7; noRuang++){
+    if(noRuang <= 3){
+        console.log(`${noRuang} berfungsi`);
+    } else if(noRuang <=4){
+        console.log(`${noRuang} sedang diperbaiki`);
+    } else {
+        console.log(`${noRuang} tidak berfungsi`);
+    }
+}
